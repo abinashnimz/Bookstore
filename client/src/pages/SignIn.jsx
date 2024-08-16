81a76710a8130ab7e5c8761ed592ec3e43f348c4
abinashnimz <abinash443steam@gmail.com>
2024-08-16 22:58:55 +0530

    added signup, signin UI

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,5 +1,29 @@
+import { Link } from "react-router-dom";
+
 export const SignIn = ()=>{
     return(
-        <h1>SignIn</h1>
+        <div className="bg-zinc-900 min-h-screen p-8 flex items-center justify-center">
+            <div className="bg-zinc-800 md:w-2/6 text-zinc-300 rounded p-4 md:w-1.5/6 md:p-12">
+                <h1 className="text-xl font-semibold mb-4 md:text-2xl">Sign In</h1>
+                <form className="flex flex-col gap-4">
+                    <div className="flex flex-col gap-1 text-zinc-500">
+                        <label htmlFor="email" className="capitalize">email</label>
+                        <input type="email" id="email" name="email" placeholder="xyz@example.com" className="bg-zinc-900 rounded px-2 py-1 text-zinc-500 md:px-4 md:py-2" />
+                    </div>
+                    <div className="flex flex-col gap-1 text-zinc-500">
+                        <label htmlFor="password" className="capitalize">password</label>
+                        <input type="password" id="password" name="password" placeholder="password" className="bg-zinc-900 rounded px-2 py-1 text-zinc-500 md:px-4 md:py-2" />
+                    </div>
+                    <div className="flex gap-2 flex-col items-center p-4">
+                        <button type="submit" className="bg-blue-500 px-4 py-2 rounded font-semibold hover:bg-zinc-200 hover:text-zinc-800 transition-all duration-300">Signin</button>
+                        <p className="mt-2 flex flex-row gap-2 text-zinc-400 text-sm">
+                            Don't have an account?
+                            <Link to={"/sign-up"} className="hover:text-zinc-300 underline">Sign Up</Link>
+                        </p>
+
+                    </div>
+                </form>
+            </div>
+        </div>
     )
 }
