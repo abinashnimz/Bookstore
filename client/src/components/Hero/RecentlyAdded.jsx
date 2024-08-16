7ae7899e817e572fe86abd289eac7f3b64a14d65
abinashnimz <abinash443steam@gmail.com>
2024-08-16 14:09:15 +0530

    added allbook page

--- client/src/components/Hero/RecentlyAdded.jsx
+++ client/src/components/Hero/RecentlyAdded.jsx
@@ -3,32 +3,32 @@ import axios from "axios";
 import { BookCard } from "../BookCard/BookCard";
 
 
-export const RecentlyAdded = ()=>{
+export const RecentlyAdded = () => {
 
     const [bookData, setBookData] = useState([]);
 
-    useEffect(()=>{
-        const fetch = async ()=>{
-            try{
+    useEffect(() => {
+        const fetch = async () => {
+            try {
                 const response = await axios.get("http://localhost:2000/api/book/recentBooks");
-                if(response){
+                if (response) {
                     setBookData(response.data.data);
                 }
                 console.log(response);
-            }catch(err){
+            } catch (err) {
                 console.log(err);
             }
         }
         fetch();
-    },[]);
+    }, []);
 
-    return(
+    return (
         <div className="mt-4 px-8 pb-8">
             <h2 className="text-2xl text-yellow-100 font-semibold md:text-3xl">Recently Added Books</h2>
             <div className="my-8 grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-4">
-                {bookData && bookData.map((item, ind)=>(
+                {bookData && bookData.map((item, ind) => (
                     <div key={ind}>
-                        <BookCard props={item}/>
+                        <BookCard props={item} />
                     </div>
                 ))}
             </div>
--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -1,5 +1,39 @@
-export const Books = ()=>{
-    return(
-        <h1>Books</h1>
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { BookCard } from "../components/BookCard/BookCard";
+
+export const Books = () => {
+
+    const [bookData, setBookData] = useState([]);
+
+    useEffect(() => {
+        const fetch = async () => {
+            try {
+                const response = await axios.get("http://localhost:2000/api/book/getBooks");
+                console.log(response);
+                if (response) {
+                    setBookData(response.data.data);
+                }
+                console.log(response);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        fetch();
+    }, []);
+
+    return (
+        <div className="bg-zinc-900 min-h-screen">
+            <div className="pt-4 px-8 pb-8">
+                <h2 className="text-2xl text-yellow-100 font-semibold md:text-3xl">All Books</h2>
+                <div className="my-8 grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-4">
+                    {bookData && bookData.map((item, ind) => (
+                        <div key={ind}>
+                            <BookCard props={item} />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
     )
 }
